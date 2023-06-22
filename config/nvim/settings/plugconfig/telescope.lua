b8da54b5402fc7156e8d3fb1c9852070f58b2dbc
higuchi-yuya-ab <higuchi_yuya@applibot.co.jp>
2023-06-22 21:40:04 +0900

    fix

--- config/nvim/settings/plugconfig/telescope.lua
+++ config/nvim/settings/plugconfig/telescope.lua
@@ -5,6 +5,8 @@ vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>b', builtin.buffers, {})
 vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>r', builtin.grep_string, {})
+vim.keymap.set('n', '<leader>q', builtin.grep_string, {})
+vim.api.nvim_set_keymap("n", "<leader><leader>", "<Cmd>lua require('telescope').extensions.frecency.frecency()<CR>", {noremap = true, silent = true})
 
 local actions = require("telescope.actions")
 require("telescope").setup{
