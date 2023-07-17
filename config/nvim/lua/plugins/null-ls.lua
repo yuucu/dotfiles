a8bab99f48e0b190c85204d13ec7851e0c5261ce
ch1hayu <39527561+yuucu@users.noreply.github.com>
2023-07-17 23:46:43 +0900

    null lsの挙動修正 (#7)
    Co-authored-by: higuchi-yuya-ab <higuchi_yuya@applibot.co.jp>

--- config/nvim/lua/plugins/null-ls.lua
+++ config/nvim/lua/plugins/null-ls.lua
@@ -6,22 +6,19 @@ return {
         local null_ls = require('null-ls')
         local sources = {
             null_ls.builtins.diagnostics.cspell.with({
+                method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
                 diagnostics_postprocess = function(diagnostic)
                     -- レベルをWARNに変更（デフォルトはERROR）
                     diagnostic.severity = vim.diagnostic.severity["WARN"]
-                end,
-                condition = function()
-                    -- cspellが実行できるときのみ有効
-                    return vim.fn.executable('cspell') > 0
                 end,
                 -- 起動時に設定ファイル読み込み
                 extra_args = { '--config', '~/.config/cspell/cspell.json' },
             }),
             null_ls.builtins.diagnostics.hadolint,
-            null_ls.builtins.formatting.terraform_fmt,
-            null_ls.builtins.diagnostics.terraform_validate,
-            null_ls.builtins.formatting.stylua,
-            null_ls.builtins.formatting.gofumpt,
+            -- null_ls.builtins.formatting.terraform_fmt,
+            -- null_ls.builtins.diagnostics.terraform_validate,
+            -- null_ls.builtins.formatting.stylua,
+            -- null_ls.builtins.formatting.gofumpt,
             null_ls.builtins.diagnostics.golangci_lint,
         }
         null_ls.setup({
