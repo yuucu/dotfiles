248a9e698bc0fb155aff91f3da781b2f46aeb6f8
higuchi-yuya-ab <higuchi_yuya@applibot.co.jp>
2023-09-04 03:06:17 +0900

    refactor

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -10,60 +10,32 @@ return {
     { "<leader>g", "<cmd>Telescope live_grep<cr>",  desc = "search by [G]rep" },
   },
   dependencies = {
-    'nvim-lua/plenary.nvim',
-    {
-      cond = false,
-      "nvim-telescope/telescope-frecency.nvim",
-      config = function()
-        require "telescope".load_extension("frecency")
-      end,
-      dependencies = { "kkharji/sqlite.lua" }
-    },
-    {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      config = function()
-        require("telescope").load_extension("fzf")
-      end,
-      cond = function()
-        return vim.fn.executable 'make' == 1
-      end,
-    }
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    -- [[ Configure Telescope ]]
-    -- See `:help telescope` and `:help telescope.setup()`
-    require("telescope").setup {
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
+    telescope.setup({
       defaults = {
+        path_display = { "truncate " },
         mappings = {
           i = {
-            ['<C-u>'] = false,
-            ['<C-d>'] = false,
+            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
           },
         },
       },
-      extensions = {
-        fzf = {
-          fuzzy = true,                   -- false will only do exact matching
-          override_generic_sorter = true, -- override the generic sorter
-          override_file_sorter = true,    -- override the file sorter
-          case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
-          -- the default case_mode is "smart_case"
-        }
-      }
-    }
-
-    -- Enable telescope fzf native, if installed
-    pcall(require('telescope').load_extension, 'fzf')
-
+    })
+    telescope.load_extension("fzf")
     -- See `:help telescope.builtin`
     -- vim.keymap.set('n', '<leader>f', require('telescope.builtin').find_files, { desc = 'search [F]iles' })
     -- vim.keymap.set('n', '<leader>g', require('telescope.builtin').live_grep, { desc = 'search by [G]rep' })
+    -- vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
+    -- vim.keymap.set('n', '<leader>b', require('telescope.builtin').buffers, { desc = 'Find existing [B]uffers' })
     vim.keymap.set('n', '<leader>h', require('telescope.builtin').help_tags, { desc = 'search [H]elp' })
     vim.keymap.set('n', '<leader>w', require('telescope.builtin').grep_string, { desc = 'search current [W]ord' })
-    -- vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
     vim.keymap.set('n', '<leader>d', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-    -- vim.keymap.set('n', '<leader>b', require('telescope.builtin').buffers, { desc = 'Find existing [B]uffers' })
     vim.api.nvim_set_keymap("n", "<leader><leader>", "<Cmd>lua require('telescope').extensions.frecency.frecency()<CR>",
       { noremap = true, silent = true })
   end,
