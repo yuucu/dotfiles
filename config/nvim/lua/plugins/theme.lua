a5f57c22c6252233d99a8f0d0a07ccfec6cddd2a
higuchi-yuya-ab <higuchi_yuya@applibot.co.jp>
2023-09-05 02:13:48 +0900

    fix: catppuccin config

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -40,6 +40,26 @@ return {
     opts = {
       term_colors = true,
       transparent_background = true,
+      integrations = {
+        alpha = true,
+        cmp = true,
+        gitsigns = true,
+        lsp_trouble = true,
+        mason = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        telescope = true,
+        treesitter = true,
+        which_key = true,
+      },
     },
     config = function()
       if vim.g.vscode then
