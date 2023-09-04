10812d8141fcefd69c9fcb7f65a2f1f5849cddab
higuchi-yuya-ab <higuchi_yuya@applibot.co.jp>
2023-09-05 01:21:44 +0900

    fix: gitssigns

--- config/nvim/lua/plugins/gitssigns.lua
+++ config/nvim/lua/plugins/gitssigns.lua
@@ -8,23 +8,12 @@ return {
   opts = {
     -- See `:help gitsigns.txt`
     signs = {
-      -- add = { text = '+' },
-      -- change = { text = '~' },
-      -- delete = { text = '_' },
-      -- topdelete = { text = '‾' },
-      -- changedelete = { text = '~' },
       add          = { text = '┆' },
       change       = { text = '┆' },
-      delete       = { text = '_' },
-      topdelete    = { text = '‾' },
+      delete       = { text = "" },
+      topdelete    = { text = "" },
       changedelete = { text = '~' },
       untracked    = { text = '┆' },
     },
-    on_attach = function(bufnr)
-      -- vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk,
-      -- { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
-      -- vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
-      -- vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
-    end,
   },
 }
