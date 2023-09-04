307f21bd2e559ecd14714cb5d31f65643131b011
higuchi-yuya-ab <higuchi_yuya@applibot.co.jp>
2023-09-05 01:17:48 +0900

    add: spectre

--- config/nvim/lua/plugins/test.lua
+++ config/nvim/lua/plugins/test.lua
@@ -53,27 +53,6 @@ return {
     end
   },
   {
-    'nvim-pack/nvim-spectre',
-    dependencies = {
-      'nvim-lua/plenary.nvim'
-    },
-    lazy = false,
-    config = function()
-      vim.keymap.set('n', '<leader>ss', '<cmd>lua require("spectre").toggle()<CR>', {
-        desc = "Toggle Spectre"
-      })
-      vim.keymap.set('n', '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
-        desc = "Search current word"
-      })
-      vim.keymap.set('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', {
-        desc = "Search current word"
-      })
-      vim.keymap.set('n', '<leader>sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
-        desc = "Search on current file"
-      })
-    end
-  },
-  {
     "mbbill/undotree",
     cmd = {
       "UndotreeToggle",
