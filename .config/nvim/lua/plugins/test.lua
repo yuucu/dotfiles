7a22658c3cdd3797cc577098557c5a4f74bb0904
higuchi-yuya-ab <higuchi_yuya@applibot.co.jp>
2023-09-10 18:01:44 +0900

    fix delete cmd

--- .config/nvim/lua/plugins/test.lua
+++ .config/nvim/lua/plugins/test.lua
@@ -35,6 +35,8 @@ return {
     },
     config = function(_, opts)
       require('leap').add_default_mappings()
+      vim.keymap.del({ 'x', 'o' }, 'x')
+      vim.keymap.del({ 'x', 'o' }, 'X')
     end,
   },
   {
