8afe9d75728d0876702d18980224da2ddfaa6a4c
higuchi-yuya-ab <higuchi_yuya@applibot.co.jp>
2023-09-18 23:13:31 +0900

    update

--- .config/nvim/lua/plugins/test.lua
+++ .config/nvim/lua/plugins/test.lua
@@ -50,4 +50,14 @@ return {
       },
     },
   },
+  {
+    'kat0h/bufpreview.vim',
+    build = 'deno task prepare',
+    ft = {
+      "markdown",
+    },
+    dependencies = {
+      'vim-denops/denops.vim'
+    }
+  }
 }
