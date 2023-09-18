b13e4979230a5bc295d4aa074032302ff0f522aa
higuchi-yuya-ab <higuchi_yuya@applibot.co.jp>
2023-09-18 23:13:31 +0900

    update

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -2,7 +2,8 @@ return {
   "github/copilot.vim",
   event = "InsertEnter",
   cond = function()
-    return not vim.g.vscode
+    -- return not vim.g.vscode
+    return false
   end,
   filetypes = {
     markdown = true,
