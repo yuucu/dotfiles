5c274344bcca77b4ab7a34ed26909ee32d3a5644
yuucu <yuucu.sub@gmail.com>
2023-11-26 04:04:58 +0900

    feat: add plugin harpoon

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -79,7 +79,7 @@ return {
     -- vim.keymap.set('n', '<leader>g', require('telescope.builtin').live_grep, { desc = 'search by [G]rep' })
     -- vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
     -- vim.keymap.set('n', '<leader>b', require('telescope.builtin').buffers, { desc = 'Find existing [B]uffers' })
-    vim.keymap.set('n', '<leader>h', require('telescope.builtin').help_tags, { desc = 'search [H]elp' })
+    -- vim.keymap.set('n', '<leader>h', require('telescope.builtin').help_tags, { desc = 'search [H]elp' })
     vim.keymap.set('n', '<leader>w', require('telescope.builtin').grep_string, { desc = 'search current [W]ord' })
     vim.keymap.set('n', '<leader>g', require "telescope".extensions.egrepify.egrepify, { desc = 'search by [G]rep' })
     -- vim.keymap.set('n', '<leader>g', require "telescope".extensions.egrepify.egrepify {}, { desc = 'search by [G]rep' })
