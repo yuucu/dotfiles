6c5ec090a89b37df8841a0b5670505d946715f0c
yuucu <yuucu.sub@gmail.com>
2023-12-10 06:43:00 +0900

    Add nvim-cmp and nvim-lspconfig plugins

--- .config/nvim/lua/plugins/neodev.lua
+++
@@ -1,9 +0,0 @@
-return {
-  -- Additional lua configuration, makes nvim stuff amazing!
-  'folke/neodev.nvim',
-  cond = false,
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    require('neodev').setup()
-  end
-}
