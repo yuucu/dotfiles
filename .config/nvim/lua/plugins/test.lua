43e970a16c21dea10d023184e3c4e4b433f8a3fa
higuchi-yuya-ab <higuchi_yuya@applibot.co.jp>
2024-07-04 02:31:21 +0900

    add: copilot chat pluginの導入

--- .config/nvim/lua/plugins/test.lua
+++ .config/nvim/lua/plugins/test.lua
@@ -53,5 +53,19 @@ return {
       'RainbowDelimQuoted',
       'RainbowMultiDelim'
     }
-  }
+  },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    lazy = false,
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" },  -- for curl, log wrapper
+    },
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
 }
