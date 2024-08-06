17a95b49a6b30fc41c02c489cf3ded2722ad54a2
higuchi-yuya-ab <higuchi_yuya@applibot.co.jp>
2024-08-06 09:14:43 +0900

    feat: trouble vimにkeys追加

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -4,4 +4,11 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {
   },
+  keys = {
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+  },
 }
