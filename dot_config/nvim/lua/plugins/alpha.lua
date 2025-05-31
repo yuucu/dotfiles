14c844c3c5dd82e3b84dfada20fe779cdd12c515
yuucu <yuucu.work@gmail.com>
2025-05-31 21:46:25 +0900

    fix: config path

--- dot_config/nvim/lua/plugins/alpha.lua
+++ dot_config/nvim/lua/plugins/alpha.lua
@@ -127,7 +127,7 @@ return {
         button('n', '📝  New Note', ':lua _G.create_new_note()<CR>'),
         button('d', '💼  Documents', ':lua _G.search_documents()<CR>'),
         button('t', '📅  Daily', ':lua _G.create_daily_note()<CR>'),
-        button('c', '🔧  Config', ':e $HOME/.config/nvim/init.lua | :cd %:p:h<CR>'),
+        button('c', '🔧  Config', ':e $HOME/ghq/github.com.yuucu/yuucu/dotfiles/dot_config/nvim/init.lua | :cd %:p:h<CR>'),
         button('q', '😶‍🌫️  Quit', ':qa<CR>'),
       },
       opts = {
