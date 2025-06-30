8739bd22901e23b3adb88d1b6537ccb88a0fc976
yuucu <yuucu.work@gmail.com>
2025-07-01 00:07:12 +0900

    test

--- dot_config/nvim/lua/plugins/nvim-cmp.lua
+++ dot_config/nvim/lua/plugins/nvim-cmp.lua
@@ -2,28 +2,22 @@ return {
   'hrsh7th/nvim-cmp',
   event = { 'InsertEnter', 'CmdlineEnter' },
   dependencies = {
-    { 'hrsh7th/cmp-nvim-lsp' },
+    'hrsh7th/cmp-nvim-lsp',
     {
       'L3MON4D3/LuaSnip',
       make = 'make install_jsregexp',
-      dependencies = { 'rafamadriz/friendly-snippets' },
-      config = function()
-        require('luasnip.loaders.from_vscode').lazy_load()
-      end,
+      dependencies = 'rafamadriz/friendly-snippets',
+      config = function() require('luasnip.loaders.from_vscode').lazy_load() end,
     },
-    { 'saadparwaiz1/cmp_luasnip' },
-    { 'hrsh7th/cmp-nvim-lua' },
-    { 'hrsh7th/cmp-path' },
-    { 'hrsh7th/cmp-buffer' },
+    'saadparwaiz1/cmp_luasnip',
+    'hrsh7th/cmp-nvim-lua',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-buffer',
   },
   config = function()
     local cmp = require('cmp')
     cmp.setup({
-      snippet = {
-        expand = function(args)
-          require('luasnip').lsp_expand(args.body)
-        end,
-      },
+      snippet = { expand = function(args) require('luasnip').lsp_expand(args.body) end },
       sources = {
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
@@ -40,12 +34,8 @@ return {
         ['<C-n>'] = cmp.mapping.select_next_item(),
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
       },
-      completion = {
-        completeopt = 'menu,menuone,preview,noselect',
-      },
-      formatting = {
-        fields = { 'abbr', 'kind', 'menu' },
-      },
+      completion = { completeopt = 'menu,menuone,preview,noselect' },
+      formatting = { fields = { 'abbr', 'kind', 'menu' } },
     })
   end,
 }
