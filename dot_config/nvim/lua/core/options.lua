bb0f3403694da5ebf68420d2f2dfaa44656b5250
yuucu <yuucu.work@gmail.com>
2025-07-10 19:02:12 +0900

    mdc

--- dot_config/nvim/lua/core/options.lua
+++ dot_config/nvim/lua/core/options.lua
@@ -48,9 +48,17 @@ vim.o.expandtab = true
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 
+-- Set mdc files as markdown filetype
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  pattern = '*.mdc',
+  callback = function()
+    vim.bo.filetype = 'markdown'
+  end,
+})
+
 -- Set markdown tab width
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'markdown',
+  pattern = { 'markdown', 'mdc' },
   callback = function()
     vim.opt_local.tabstop = 2
     vim.opt_local.shiftwidth = 2
