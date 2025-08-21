632a7744b6a0ce1998d8ccccebc1f8858c02e475
yuucu <yuucu.work@gmail.com>
2025-08-21 13:39:03 +0900

    add: PlantUMLプレビューアーを追加
    - weirongxu/plantuml-previewer.vimプラグインを追加
    - open-browser.vim依存関係を追加
    - PlantUMLファイル用のキーマップを設定:
      - <leader>pp: プレビューを開く
      - <leader>ps: 画像として保存
      - <leader>pt: プレビューをトグル
    - PlantUMLプレビューアーの基本設定を追加

--- dot_config/nvim/lua/plugins/plantuml.lua
+++ dot_config/nvim/lua/plugins/plantuml.lua
@@ -1,25 +1,48 @@
 return {
-  -- PlantUML syntax highlighting
-  'aklt/plantuml-syntax',
-  ft = { 'plantuml', 'puml' },
-  config = function()
-    -- PlantUMLファイルの設定
-    vim.api.nvim_create_autocmd('FileType', {
-      pattern = 'plantuml',
-      callback = function()
-        -- インデント設定
-        vim.bo.shiftwidth = 2
-        vim.bo.tabstop = 2
-        vim.bo.expandtab = true
-        vim.bo.softtabstop = 2
-        
-        -- 折りたたみ設定
-        vim.wo.foldmethod = 'indent'
-        vim.wo.foldlevel = 99
-        
-        -- コメント設定
-        vim.bo.commentstring = "' %s"
-      end,
-    })
-  end,
+  {
+    -- PlantUML syntax highlighting
+    'aklt/plantuml-syntax',
+    ft = { 'plantuml', 'puml' },
+  },
+  {
+    -- PlantUML previewer
+    'weirongxu/plantuml-previewer.vim',
+    ft = { 'plantuml', 'puml' },
+    dependencies = {
+      'tyru/open-browser.vim',
+    },
+    config = function()
+      -- PlantUMLファイルの設定
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'plantuml',
+        callback = function()
+          -- インデント設定
+          vim.bo.shiftwidth = 2
+          vim.bo.tabstop = 2
+          vim.bo.expandtab = true
+          vim.bo.softtabstop = 2
+          
+          -- 折りたたみ設定
+          vim.wo.foldmethod = 'indent'
+          vim.wo.foldlevel = 99
+          
+          -- コメント設定
+          vim.bo.commentstring = "' %s"
+          
+          -- PlantUMLプレビュー用キーマップ
+          local opts = { buffer = true, silent = true }
+          vim.keymap.set('n', '<leader>pp', ':PlantumlOpen<CR>', opts)
+          vim.keymap.set('n', '<leader>ps', ':PlantumlSave<CR>', opts)
+          vim.keymap.set('n', '<leader>pt', ':PlantumlToggle<CR>', opts)
+        end,
+      })
+      
+      -- PlantUMLプレビューアーの設定
+      vim.g.plantuml_previewer = {
+        plantuml_jar_path = vim.fn.expand('~/plantuml.jar'),
+        save_format = 'png',
+        include_path = vim.fn.expand('~/.plantuml'),
+      }
+    end,
+  },
 }
