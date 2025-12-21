4d6cf0477bcd0f63ba0ffd3503a8d2c07d16d618
yuucu <yuucu.work@gmail.com>
2025-12-22 02:59:11 +0900

    refactor(nvim): leap.nvim を flash.nvim に移行
    より高機能な移動プラグイン flash.nvim に変更。
    
    主な機能:
    - s: 通常のジャンプ
    - S: Treesitter ノードへのジャンプ
    - r: リモートジャンプ (operator-pending)
    - R: Treesitter 検索
    - <C-s>: コマンドラインモードでの Flash トグル
    
    削除: leap.nvim
    追加: flash.nvim

---
+++ dot_config/nvim/lua/plugins/flash.lua
@@ -0,0 +1,80 @@
+return {
+  'folke/flash.nvim',
+  event = 'VeryLazy',
+  opts = {
+    labels = 'asdfghjklqwertyuiopzxcvbnm',
+    search = {
+      multi_window = false,
+      forward = true,
+      wrap = true,
+      mode = 'exact',
+    },
+    jump = {
+      jumplist = true,
+      pos = 'start',
+      history = false,
+      register = false,
+      nohlsearch = true,
+      autojump = false,
+    },
+    label = {
+      uppercase = true,
+      rainbow = {
+        enabled = false,
+        shade = 5,
+      },
+    },
+    modes = {
+      search = {
+        enabled = true,
+      },
+      char = {
+        enabled = true,
+        jump_labels = true,
+        multi_line = true,
+      },
+    },
+  },
+  keys = {
+    {
+      's',
+      mode = { 'n', 'x', 'o' },
+      function()
+        require('flash').jump()
+      end,
+      desc = 'Flash',
+    },
+    {
+      'S',
+      mode = { 'n', 'x', 'o' },
+      function()
+        require('flash').treesitter()
+      end,
+      desc = 'Flash Treesitter',
+    },
+    {
+      'r',
+      mode = 'o',
+      function()
+        require('flash').remote()
+      end,
+      desc = 'Remote Flash',
+    },
+    {
+      'R',
+      mode = { 'o', 'x' },
+      function()
+        require('flash').treesitter_search()
+      end,
+      desc = 'Treesitter Search',
+    },
+    {
+      '<c-s>',
+      mode = { 'c' },
+      function()
+        require('flash').toggle()
+      end,
+      desc = 'Toggle Flash Search',
+    },
+  },
+}
