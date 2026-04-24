25730c6f40a271b22f768664e685e5631c8c737b
yuucu <yuucu.work@gmail.com>
2026-04-24 12:31:09 +0900

    fix(nvim): migrate nvim-treesitter to main branch for Neovim 0.12 compatibility
    nvim-treesitter の master ブランチがアーカイブされ Neovim 0.12.1 の API と非互換になったため、main ブランチへ移行。configs モジュールが廃止されたため nvim-treesitter.setup() を直接呼び出す形に変更。

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,63 +1,51 @@
 return {
   -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
+  branch = 'main',
   event = { 'BufReadPost', 'BufNewFile' },
   build = ':TSUpdate',
   cmd = { 'TSUpdateSync' },
   cond = function()
     return not vim.g.vscode
   end,
-  opts = {
-    ensure_installed = {
-      'go',
-      'gosum',
-      'gomod',
-      'gowork',
-      'lua',
-      'python',
-      'rust',
-      'typescript',
-      'tsx',
-      'vimdoc',
-      'vim',
-      'kotlin',
-      'dockerfile',
-      'json',
-      'json5',
-      'jsonc',
-      'terraform',
-      'hcl',
-      'bash',
-      'c',
-      'html',
-      'javascript',
-      'jsdoc',
-      'luadoc',
-      'luap',
-      'markdown',
-      'markdown_inline',
-      'query',
-      'regex',
-      'yaml',
-      'css',
-      'scss',
-    },
-    -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-    auto_install = true,
-    highlight = { enable = true },
-    indent = { enable = true },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = '<c-space>',
-        node_incremental = '<c-space>',
-        scope_incremental = '<c-s>',
-        node_decremental = '<M-space>',
+  config = function()
+    require('nvim-treesitter').setup({
+      ensure_installed = {
+        'go',
+        'gosum',
+        'gomod',
+        'gowork',
+        'lua',
+        'python',
+        'rust',
+        'typescript',
+        'tsx',
+        'vimdoc',
+        'vim',
+        'kotlin',
+        'dockerfile',
+        'json',
+        'json5',
+        'jsonc',
+        'terraform',
+        'hcl',
+        'bash',
+        'c',
+        'html',
+        'javascript',
+        'jsdoc',
+        'luadoc',
+        'luap',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'regex',
+        'yaml',
+        'css',
+        'scss',
       },
-    },
-  },
-  config = function(_, opts)
-    require('nvim-treesitter.configs').setup(opts)
+    })
+
     -- Diagnostic keymaps
     vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
     vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
